730e12af9dfb8e843685eb9e1324bfbf556de077
jimo <jimolonely@yahoo.com>
2022-04-24 17:22:43 +0800

    add failed test

--- test/src/test/java/com/jimo/niubi/SumTest.java
+++ test/src/test/java/com/jimo/niubi/SumTest.java
@@ -12,4 +12,10 @@ public class SumTest {
         assertEquals(2, s.add(1, 1));
         assertEquals(100, s.add(-3, 103));
     }
+
+    @Test
+    public void failedTest() {
+        final Sum s = new Sum();
+        assertEquals(1, s.add(2, 3));
+    }
 }
